73cdadd6fe5bf790ea5eed51d1308c722127cd70
mariaelena <mariaelenafdez1@gmail.com>
2022-10-10 15:28:35 -0500

    Agregación de GUIs

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,48 +1,62 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/prefer-default-export */
 import React, { useContext } from 'react';
 import { context } from '../../context/authContext';
-import './Login.css'
+import './Login.css';
 import { useForm } from '../../hooks/useForm';
 
-export const Login = () => {
+export function Login() {
     const authContext = useContext(context);
     const initialForm = {
         email: '',
         password: '',
     };
-    const [ formValues, handleInputChange, reset ] = useForm( initialForm );
+    const [formValues, handleInputChange, reset] = useForm(initialForm);
 
     const handleLogin = (e) => {
         e.preventDefault();
-        fetch('http://localhost:3001/api/auth', {
+        fetch('http://localhost:8080/api/auth', {
         method: 'POST',
         body: JSON.stringify({
             email: formValues.email,
-            password:  formValues.password
+            password: formValues.password,
         }),
-        headers: {"Content-type": "application/json; charset=UTF-8"}
+        headers: { 'Content-type': 'application/json; charset=UTF-8' },
     })
-    .then(response => response.json())
-    .then(json => {
-        let isLogged = json.ok;
-        console.log(json)
-        if(isLogged){
+    .then((response) => response.json())
+    .then((json) => {
+        const isLogged = json.data.logged;
+        // eslint-disable-next-line no-console
+        console.log(json);
+        if (isLogged) {
             authContext.setLogged(true);
+        } else {
+            // eslint-disable-next-line no-alert
+            alert('usuario no existe');
         }
-        else{
-            alert("usuario no existe")
-        }
-    })
-    }
+    });
+    };
   return (
-    <>
-        <div className="login">
-            <h1>Login</h1>
-            <form method="post">
-                <input type="text" onChange={handleInputChange} name="email" placeholder="email" required="required" />
-                <input type="password" onChange={handleInputChange} name="password" placeholder="Password" required="required" />
-                <button type="submit" onClick={handleLogin} class="btn btn-primary btn-block btn-large">Let me in.</button>
-            </form>
+    <div className="center">
+      <h1>¡Bienvenido!</h1>
+      <form method="post">
+        <div className="txt_field">
+          <input type="text" name="email" id="email" required />
+          <label> Correo Electrónico</label>
         </div>
-    </>
-  )
+        <div className="txt_field">
+          <input type="password" name="password" id="password" required />
+          <label>Contraseña</label>
+        </div>
+        <div className="pass">¿Olvidaste la contraseña?</div>
+        <input type="submit" value="Login" />
+        <div className="signup_link">
+          Aún no eres miembro?
+          <a href="#"> Regístrate</a>
+        </div>
+      </form>
+    </div>
+  );
 }
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,7 +1,88 @@
-import React from 'react'
-
-export const Register = () => {
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
+import './Register.css';
+// eslint-disable-next-line import/prefer-default-export
+export function Register() {
   return (
-    <div>Register</div>
-  )
+    <div className="container">
+      <div className="title">Crear cuenta</div>
+      <form action="#">
+        <div className="user-details">
+          <div className="input-box">
+            <span className="details">Nombre</span>
+            <input type="text" placeholder="Ingresa tu nombre" required />
+          </div>
+          <div className="input-box">
+            <span className="details">Apellido</span>
+            <input type="text" placeholder="Ingresa tu apellido" required />
+          </div>
+          <div className="input-box">
+            <span className="details">Peso</span>
+            <input type="text" placeholder="Ingresa tu peso" required />
+          </div>
+          <div className="input-box">
+            <span className="details">Estatura</span>
+            <input type="text" placeholder="Ingresa tu estatura" required />
+          </div>
+        </div>
+        <div className="selection-details">
+          <span className="selection-title">Género</span>
+          <input type="radio" name="selection" id="dot-l" />
+          <input type="radio" name="selection" id="dot-2" />
+          <input type="radio" name="selection" id="dot-3" />
+          <div className="category">
+            <label htmlFor="dot-l">
+              <span className="dot one" />
+              <span className="selection">Hombre</span>
+            </label>
+            <label htmlFor="dot-2">
+              <span className="dot two" />
+              <span className="selection">Mujer</span>
+            </label>
+            <label htmlFor="dot-3">
+              <span className="dot three" />
+              <span className="selection">No especificar</span>
+            </label>
+          </div>
+        </div>
+        <div className="selection-details">
+          <span className="selection-title">¿Cuántas veces a la semana realiza actividad física?</span>
+          <input type="radio" name="selection" id="dot-4" />
+          <input type="radio" name="selection" id="dot-5" />
+          <input type="radio" name="selection" id="dot-6" />
+          <div className="category">
+            <label htmlFor="dot-4">
+              <span className="dot four" />
+              <span className="selection">1 vez</span>
+            </label>
+            <label htmlFor="dot-5">
+              <span className="dot five" />
+              <span className="gender">2 veces</span>
+            </label>
+            <label htmlFor="dot-6">
+              <span className="dot six" />
+              <span className="gender">3 o más veces</span>
+            </label>
+          </div>
+        </div>
+        <div className="input-box-one">
+          <span className="details">Correo Electrónico</span>
+          <input type="text" placeholder="Ingresa tu correo electrónico" required />
+        </div>
+        <div className="user-details">
+          <div className="input-box">
+            <span className="details">Contraseña</span>
+            <input type="password" placeholder="Contraseña" required />
+          </div>
+          <div className="input-box">
+            <span className="details">Confirma la contraseña</span>
+            <input type="password" placeholder="Confirma la contraseña" required />
+          </div>
+        </div>
+        <div className="button">
+          <input type="submit" value="Crear Cuenta" />
+        </div>
+      </form>
+    </div>
+  );
 }
